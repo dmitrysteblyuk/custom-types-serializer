5855a6587eaf085fa18b0f7f778ca86a193fe9d5
Dmitry Steblyuk <dsteblyuk@matterway.io>
2021-09-04 14:16:40 +0200

    chore: initial commit - generate config files

---
+++ src/index.test.ts
@@ -0,0 +1,8 @@
+import {describe, it, expect} from '@jest/globals';
+import * as api from '.';
+
+describe('custom-types-serializer', () => {
+  it('does nothing', () => {
+    expect(api).not.toBeUndefined();
+  });
+});
